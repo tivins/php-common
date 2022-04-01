2ae510a5e857df2ef42e713497921827229c0b11
Tivins <tivins@elabo.fr>
2022-04-01 14:39:52 +0200

    Update ContentType.php

--- src/lib/Tivins/Core/Http/ContentType.php
+++ src/lib/Tivins/Core/Http/ContentType.php
@@ -4,9 +4,15 @@ namespace Tivins\Core\Http;
 
 enum ContentType: string
 {
+    case ALL = '*';
     case HTML = 'text/html';
     case TEXT = 'text/plain';
     case JS = 'text/javascript';
     case CSS = 'text/css';
     case JSON = 'application/json';
+    case JPEG = 'image/jpeg';
+    case PNG = 'image/png';
+    case GIF = 'image/gif';
+    case MP3 = 'audio/mpeg';
+    case MP4 = 'video/mp4';
 }
