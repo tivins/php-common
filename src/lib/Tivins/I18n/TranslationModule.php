7729b170248d3d039887b5a97efc527214891e78
Tivins <tivins@elabo.fr>
2022-04-01 11:36:25 +0200

    Update TranslationModule.php

--- src/lib/Tivins/I18n/TranslationModule.php
+++ src/lib/Tivins/I18n/TranslationModule.php
@@ -17,19 +17,19 @@ namespace Tivins\I18n;
  */
 abstract class TranslationModule
 {
-    public function get(string $lang): array
+    public function get(Language $lang, Language $fallbackLang = Language::English): array
     {
         $data = $this->getAll();
         $combined = [];
         foreach ($data as $key => $values) {
-            $combined[$key] = $values[$lang] ?? $values['en'] ?? '';
+            $combined[$key] = $values[$lang->value] ?? $values[$fallbackLang->value] ?? '';
         }
         return $combined;
     }
 
-    public function createI18n(string $lang): I18n
+    public function createI18n(Language $lang, Language $fallbackLang): I18n
     {
-        return new I18n($this->get($lang));
+        return new I18n($this->get($lang, $fallbackLang));
     }
 
     /**
