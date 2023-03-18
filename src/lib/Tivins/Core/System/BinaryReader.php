5d120b0f776a9e6af1f05be20a586c5e65933ac1
Tivins <479448+tivins@users.noreply.github.com>
2023-03-18 09:41:34 +0100

    Update BinaryReader.

--- src/lib/Tivins/Core/System/BinaryReader.php
+++ src/lib/Tivins/Core/System/BinaryReader.php
@@ -2,21 +2,30 @@
 
 namespace Tivins\Core\System;
 
-use RuntimeException;
+use Exception;
 
 class BinaryReader
 {
+    /**
+     * @throws Exception
+     */
     public static function open(string $filename): static
     {
         $fileHandle = fopen($filename, 'r');
         if (!$fileHandle) {
-            throw new RuntimeException('Failed to open file for reading');
+            throw new Exception('Failed to open file for reading');
         }
         return new static($fileHandle);
     }
 
+    /**
+     * @throws Exception
+     */
     public function __construct(private $fileHandler)
     {
+        if (!is_resource($this->fileHandler)) {
+            throw new Exception('fileHandler is not a resource');
+        }
     }
 
     public function close(): bool
@@ -34,26 +43,42 @@ class BinaryReader
         return fseek($this->fileHandler, $offset, $whence);
     }
 
-    public function readChars(int $length): string|false
+    /**
+     * @throws Exception
+     */
+    public function readChars(int $length): string
     {
-        return fread($this->fileHandler, $length);
+        $data = fread($this->fileHandler, $length);
+        if ($data === false) {
+            throw new Exception();
+        }
+        return $data;
     }
 
-    // v unsigned short (always 16 bit, little endian byte order)
+    /**
+     * v unsigned short (always 16 bit, little endian byte order)
+     * @throws Exception
+     */
     public function readU16LE(): int
     {
         $bytes = $this->readChars(2);
         return unpack('v', $bytes)[1];
     }
 
-    // V unsigned long (always 32 bit, little endian byte order)
+    /**
+     * V unsigned long (always 32 bit, little endian byte order)
+     * @throws Exception
+     */
     public function readU32LE(): int
     {
         $bytes = $this->readChars(4);
         return unpack('V', $bytes)[1];
     }
 
-    // P unsigned long long (always 64 bit, little endian byte order)
+    /**
+     * P unsigned long long (always 64 bit, little endian byte order)
+     * @throws Exception
+     */
     public function readU64LE(): int
     {
         $bytes = $this->readChars(8);
